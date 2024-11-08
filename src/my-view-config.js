8df8b38300cdecb1db3154353fa1413d8de702fb
chenwenchang <479018666@qq.com>
2024-11-08 19:57:39 +0800

    debug spatial

--- src/my-view-config.js
+++ src/my-view-config.js
@@ -1,45 +1,36 @@
-{
+const vitessceConfig = {
     "version": "1.0.17",
     "name": "Wenchang test",
     "description": "Spatial Visualization Demo",
-    "datasets":
-    [
+    "datasets": [
         {
             "uid": "test1",
             "name": "test1",
-            "files":
-            [
+            "files": [
                 {
                     "fileType": "anndata.zarr",
                     "url": "https://chenwenchang.github.io/vitessce-demo-gh-pages/breast_spatial_sample1.zarr",
-                    "coordinationValues":
-                    {
+                    "coordinationValues": {
                         "obsType": "bead",
                         "featureType": "gene",
                         "featureValueType": "expression",
                         "embeddingType": "UMAP"
                     },
-                    "options":
-                    {
-                        "obsFeatureMatrix":
-                        {
+                    "options": {
+                        "obsFeatureMatrix": {
                             "path": "obsm/X_hvg",
                             "featureFilterPath": "var/highly_variable"
                         },
-                        "obsEmbedding":
-                        {
+                        "obsEmbedding": {
                             "path": "obsm/X_umap"
                         },
-                        "obsLocations":
-                        {
+                        "obsLocations": {
                             "path": "obsm/X_spatial"
                         },
-                        "image":
-                        {
+                        "image": {
                             "path": "uns/spatial/HBCA12_spara1-2_ref382020/images"
                         },
-                        "obsSets":
-                        [
+                        "obsSets": [
                             {
                                 "name": "Cell Type",
                                 "path": "obs/cell_type"
@@ -65,8 +56,7 @@
                                 "path": "obs/donor_menopausal_status"
                             }
                         ],
-                        "featureLabels":
-                        {
+                        "featureLabels": {
                             "path": "var/feature_name"
                         }
                     }
@@ -75,36 +65,27 @@
         }
     ],
     "initStrategy": "auto",
-    "coordinationSpace":
-    {
-        "embeddingType":
-        {
+    "coordinationSpace": {
+        "embeddingType": {
             "UMAP": "UMAP"
         },
-        "obsType":
-        {
+        "obsType": {
             "A": "bead"
         },
-        "featureType":
-        {
+        "featureType": {
             "A": "gene"
         },
-        "featureValueType":
-        {
+        "featureValueType": {
             "A": "expression"
         },
-        "featureValueColormapRange":
-        {
-            "A":
-            [
+        "featureValueColormapRange": {
+            "A": [
                 0,
                 0.1
             ]
         },
-        "spatialSegmentationLayer":
-        {
-            "A":
-            {
+        "spatialSegmentationLayer": {
+            "A": {
                 "opacity": 1,
                 "radius": 16.74,
                 "visible": true,
@@ -112,16 +93,14 @@
             }
         }
     },
-    "layout":
-    [
+    "layout": [
         {
             "component": "obsSets",
             "h": 8,
             "w": 2,
             "x": 0,
             "y": 0,
-            "coordinationScopes":
-            {
+            "coordinationScopes": {
                 "obsType": "A"
             },
             "uid": "A"
@@ -132,8 +111,7 @@
             "w": 4,
             "x": 2,
             "y": 0,
-            "coordinationScopes":
-            {
+            "coordinationScopes": {
                 "embeddingType": "UMAP",
                 "obsType": "A",
                 "featureType": "A",
@@ -148,8 +126,7 @@
             "w": 4,
             "x": 6,
             "y": 0,
-            "coordinationScopes":
-            {
+            "coordinationScopes": {
                 "obsType": "A",
                 "featureType": "A",
                 "featureValueType": "A",
@@ -164,12 +141,11 @@
             "w": 2,
             "x": 10,
             "y": 0,
-            "coordinationScopes":
-            {
+            "coordinationScopes": {
                 "obsType": "A",
                 "featureType": "A"
             },
             "uid": "G"
         }
     ]
-}
+};
